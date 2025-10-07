0713f25a59b1bc165749dad893254566dc4add49
Tarpan Das <tarpandasstudy@gmail.com>
2025-10-07 20:17:37 +0530

    Added and/or updated files in _015_classes directory.

--- _015_classes/class_methods.cpp
+++ _015_classes/class_methods.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class myClass1 {
+    public:
+        // Method/function definition inside the class
+        void printHello() {
+            cout << "Hi World!" << endl;
+        }
+};
+class myClass2 {
+    public:
+        void printHello();
+};
+
+// Method/function definition outside the class
+void myClass2::printHello(){
+    cout << "Hello World!" << endl;
+}
 int main() {
     // "Here, we understand about Methods in classes."
     
+    /*
+    Methods are functions which are present in the class.
+
+    Methods can be of two types:
+    1. Inside class definition
+    2. Outside class definition
+    */
+    myClass1 obj1;
+    obj1.printHello();
+
+    myClass2 obj2;
+    obj2.printHello();
+
     return 0;
 }
---
+++ _015_classes/constructors.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Here, we understand about constructors.
+
+    /*
+        Constructors are special methods that are automatically created when an object of a class is created.
+        To create a constructor, we use the same name as class and use a paranthesis.    
+    */
+    
+
+    return 0;
+}
